1d2e4fc1c6d73d2d7413c5e71ec5a56317c9a97e
mrange <marten_range@hotmail.com>
2013-02-19 20:57:00 +0100

    1. Added WPF/BindingCache.cs 2. Added WPF/BasicBindings.cs

---
+++ WPF/BindingCache.cs
@@ -0,0 +1,45 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+namespace Source.WPF
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Windows.Data;
+
+    sealed partial class BindingCache<TKey, TBinding>
+        where TBinding : BindingBase
+    {
+        readonly ConcurrentDictionary<TKey, TBinding> m_cache = new ConcurrentDictionary<TKey, TBinding>();
+        readonly Func<TKey, TBinding> m_bindingFactory;
+
+        public BindingCache(Func<TKey, TBinding> bindingFactory)
+        {
+            if (bindingFactory == null)
+            {
+                throw new ArgumentNullException("bindingFactory");
+            }
+
+            m_bindingFactory = bindingFactory;
+        }
+
+        public TBinding GetOrAdd(TKey key)
+        {
+            return m_cache.GetOrAdd(key, m_bindingFactory);
+        }
+
+        public object ProvideValue(TKey key, IServiceProvider serviceProvider)
+        {
+            return GetOrAdd(key).ProvideValue(serviceProvider);
+        }
+    }
+}
---
+++ WPF/Bindings/BasicBindings.cs
@@ -0,0 +1,98 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+// ReSharper disable PartialTypeWithSinglePart
+
+// ### INCLUDE: ../BindingCache.cs
+
+namespace Source.WPF.Bindings
+{
+    using System;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    sealed partial class OneTime : MarkupExtension
+    {
+        static readonly BindingCache<string, Binding> s_cache = new BindingCache<string, Binding>(
+            k => new Binding(k) { Mode=BindingMode.OneTime,}
+            );
+
+        public readonly string Path;
+
+        public OneTime(string path)
+        {
+            Path = path ?? "";
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return s_cache.ProvideValue(Path, serviceProvider);
+        }
+    }
+
+    sealed partial class OneWay : MarkupExtension
+    {
+        static readonly BindingCache<string, Binding> s_cache = new BindingCache<string, Binding>(
+            k => new Binding(k) { Mode = BindingMode.OneWay, ValidatesOnDataErrors = true,}
+            );
+
+        public readonly string Path;
+
+        public OneWay(string path)
+        {
+            Path = path ?? "";
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return s_cache.ProvideValue(Path, serviceProvider);
+        }
+    }
+
+    sealed partial class TwoWay : MarkupExtension
+    {
+        static readonly BindingCache<string, Binding> s_cache = new BindingCache<string, Binding>(
+            k => new Binding(k) { Mode = BindingMode.TwoWay, ValidatesOnDataErrors = true, }
+            );
+
+        public readonly string Path;
+
+        public TwoWay(string path)
+        {
+            Path = path ?? "";
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return s_cache.ProvideValue(Path, serviceProvider);
+        }
+    }
+
+    sealed partial class Immediate : MarkupExtension
+    {
+        static readonly BindingCache<string, Binding> s_cache = new BindingCache<string, Binding>(
+            k => new Binding(k) { Mode = BindingMode.TwoWay, ValidatesOnDataErrors = true, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged, }
+            );
+
+        public readonly string Path;
+
+        public Immediate(string path)
+        {
+            Path = path ?? "";
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return s_cache.ProvideValue(Path, serviceProvider);
+        }
+    }
+}
