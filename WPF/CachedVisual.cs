8edbdfbf386bb272a1846a4ffe6b52b95a6e4220
mrange <marten_range@hotmail.com>
2013-03-25 07:03:19 +0100

    1. Began working on CachedVisual

---
+++ WPF/CachedVisual.cs
@@ -0,0 +1,84 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+namespace Source.WPF
+{
+    using Source.Extensions;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Markup;
+    using System.Windows.Media;
+
+    partial class CachedVisual : FrameworkElement, IAddChild
+    {
+        public enum Strategy
+        {
+            None    ,
+            Drawing ,
+            Bitmap  ,
+        }
+
+        public void AddChild(object value)
+        {
+            Decorator x;
+            var uiElement = value as UIElement;
+            if (uiElement != null)
+            {
+                Child = uiElement;
+            }
+        }
+
+        public void AddText(string text)
+        {
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get
+            {
+                return Child != null ? 1 : 0;
+            }
+        }
+        protected override Visual GetVisualChild(int index)
+        {
+            return index == 0 ? Child : null;
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var child = Child;
+            if (child == null)
+            {
+                return base.MeasureOverride(availableSize);
+            }
+
+            child.Measure(availableSize);
+            return child.DesiredSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var child = Child;
+            if (child == null)
+            {
+                return base.MeasureOverride(finalSize);
+            }
+
+            child.Arrange(finalSize.ToRect());
+            return child.RenderSize;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+        }
+    }
+}
---
+++ WPF/Generated_CachedVisual_DependencyProperties.cs
@@ -0,0 +1,193 @@
+﻿
+// ############################################################################
+// #                                                                          #
+// #        ---==>  T H I S  F I L E  I S   G E N E R A T E D  <==---         #
+// #                                                                          #
+// # This means that any edits to the .cs file will be lost when its          #
+// # regenerated. Changes should instead be applied to the corresponding      #
+// # template file (.tt)                                                      #
+// ############################################################################
+
+
+
+                                   
+
+// ReSharper disable CompareOfFloatsByEqualityOperator
+// ReSharper disable InconsistentNaming
+// ReSharper disable InvocationIsSkipped
+// ReSharper disable PartialMethodWithSinglePart
+// ReSharper disable PartialTypeWithSinglePart
+// ReSharper disable PossibleUnintendedReferenceComparison
+// ReSharper disable RedundantAssignment
+// ReSharper disable RedundantCast
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable UnusedMember.Local
+
+namespace Source.WPF
+{
+    using System.Collections;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+
+    using System.Windows;
+    using System.Windows.Media;
+
+    // ------------------------------------------------------------------------
+    // CachedVisual
+    // ------------------------------------------------------------------------
+    partial class CachedVisual
+    {
+        #region Uninteresting generated code
+        public static readonly DependencyProperty ChildProperty = DependencyProperty.Register (
+            "Child",
+            typeof (UIElement),
+            typeof (CachedVisual),
+            new FrameworkPropertyMetadata (
+                default (UIElement),
+                FrameworkPropertyMetadataOptions.None,
+                Changed_Child,
+                Coerce_Child          
+            ));
+
+        static void Changed_Child (DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var instance = dependencyObject as CachedVisual;
+            if (instance != null)
+            {
+                var oldValue = (UIElement)eventArgs.OldValue;
+                var newValue = (UIElement)eventArgs.NewValue;
+
+                instance.Changed_Child (oldValue, newValue);
+            }
+        }
+
+
+        static object Coerce_Child (DependencyObject dependencyObject, object basevalue)
+        {
+            var instance = dependencyObject as CachedVisual;
+            if (instance == null)
+            {
+                return basevalue;
+            }
+            var oldValue = (UIElement)basevalue;
+            var newValue = oldValue;
+
+            instance.Coerce_Child (oldValue, ref newValue);
+
+
+            return newValue;
+        }
+
+        public static readonly DependencyProperty CacheStrategyProperty = DependencyProperty.Register (
+            "CacheStrategy",
+            typeof (Strategy),
+            typeof (CachedVisual),
+            new FrameworkPropertyMetadata (
+                default (Strategy),
+                FrameworkPropertyMetadataOptions.None,
+                Changed_CacheStrategy,
+                Coerce_CacheStrategy          
+            ));
+
+        static void Changed_CacheStrategy (DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var instance = dependencyObject as CachedVisual;
+            if (instance != null)
+            {
+                var oldValue = (Strategy)eventArgs.OldValue;
+                var newValue = (Strategy)eventArgs.NewValue;
+
+                instance.Changed_CacheStrategy (oldValue, newValue);
+            }
+        }
+
+
+        static object Coerce_CacheStrategy (DependencyObject dependencyObject, object basevalue)
+        {
+            var instance = dependencyObject as CachedVisual;
+            if (instance == null)
+            {
+                return basevalue;
+            }
+            var oldValue = (Strategy)basevalue;
+            var newValue = oldValue;
+
+            instance.Coerce_CacheStrategy (oldValue, ref newValue);
+
+
+            return newValue;
+        }
+
+        #endregion
+
+        // --------------------------------------------------------------------
+        // Constructor
+        // --------------------------------------------------------------------
+        public CachedVisual ()
+        {
+            CoerceAllProperties ();
+            Constructed__CachedVisual ();
+        }
+        // --------------------------------------------------------------------
+        partial void Constructed__CachedVisual ();
+        // --------------------------------------------------------------------
+        void CoerceAllProperties ()
+        {
+            CoerceValue (ChildProperty);
+            CoerceValue (CacheStrategyProperty);
+        }
+
+
+        // --------------------------------------------------------------------
+        // Properties
+        // --------------------------------------------------------------------
+
+           
+        // --------------------------------------------------------------------
+        public UIElement Child
+        {
+            get
+            {
+                return (UIElement)GetValue (ChildProperty);
+            }
+            set
+            {
+                if (Child != value)
+                {
+                    SetValue (ChildProperty, value);
+                }
+            }
+        }
+        // --------------------------------------------------------------------
+        partial void Changed_Child (UIElement oldValue, UIElement newValue);
+        partial void Coerce_Child (UIElement value, ref UIElement coercedValue);
+        // --------------------------------------------------------------------
+
+
+           
+        // --------------------------------------------------------------------
+        public Strategy CacheStrategy
+        {
+            get
+            {
+                return (Strategy)GetValue (CacheStrategyProperty);
+            }
+            set
+            {
+                if (CacheStrategy != value)
+                {
+                    SetValue (CacheStrategyProperty, value);
+                }
+            }
+        }
+        // --------------------------------------------------------------------
+        partial void Changed_CacheStrategy (Strategy oldValue, Strategy newValue);
+        partial void Coerce_CacheStrategy (Strategy value, ref Strategy coercedValue);
+        // --------------------------------------------------------------------
+
+
+    }
+    // ------------------------------------------------------------------------
+
+}
+                                   
