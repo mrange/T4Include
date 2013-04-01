95daa27885c70d29824901e339a04588f8671808
mrange <marten_range@hotmail.com>
2013-04-01 12:49:17 +0200

    1. Basic debug functionality inplace

--- WPF/Debug/DebugVisualTreeControl.cs
+++ WPF/Debug/DebugVisualTreeControl.cs
@@ -10,11 +10,19 @@
 // You must not remove this notice, or any other, from this software.
 // ----------------------------------------------------------------------------------------------
 
+// ### INCLUDE: Generated_DebugVisualTreeControl_DependencyProperties.cs
 // ### INCLUDE: DebugContainerControl.cs
 // ### INCLUDE: ../../Extensions/WpfExtensions.cs
 
 // ReSharper disable InconsistentNaming
 
+
+using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Source.WPF.Debug
 {
@@ -22,19 +30,22 @@ namespace Source.WPF.Debug
     using System.Windows.Markup;
     using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
+    using System.Linq;
+    using Source.Extensions;
+
 
     [TemplatePart (Name = PART_SelectButton     , Type = typeof (ButtonBase))   ]
     [TemplatePart (Name = PART_RefreshButton    , Type = typeof (ButtonBase))   ]
     [TemplatePart (Name = PART_SearchBox        , Type = typeof (TextBoxBase))  ]
     [TemplatePart (Name = PART_VisualTreeView   , Type = typeof (TreeView))     ]
-    [TemplatePart (Name = PART_NodeListView     , Type = typeof (ListView))     ]
+    [TemplatePart (Name = PART_PropertyDataGrid , Type = typeof (DataGrid))     ]
     partial class DebugVisualTreeControl : DebugControl
     {
-        public const string PART_SelectButton   = @"PART_SelectButton"  ;
-        public const string PART_RefreshButton  = @"PART_RefreshButton" ;
-        public const string PART_SearchBox      = @"PART_SearchBox"     ;
-        public const string PART_VisualTreeView = @"PART_VisualTreeView";
-        public const string PART_NodeListView   = @"PART_NodeListView"  ;
+        public const string PART_SelectButton       = @"PART_SelectButton"      ;
+        public const string PART_RefreshButton      = @"PART_RefreshButton"     ;
+        public const string PART_SearchBox          = @"PART_SearchBox"         ;
+        public const string PART_VisualTreeView     = @"PART_VisualTreeView"    ;
+        public const string PART_PropertyDataGrid   = @"PART_PropertyDataGrid"  ;
 
         public const string DefaultStyle = @"
 <Style 
@@ -61,15 +72,197 @@ namespace Source.WPF.Debug
                         <Button x:Name=""PART_RefreshButton""   Padding=""8,0,8,0"" Margin=""0,0,4,0"" Content=""Refresh""/>
                     </StackPanel>
                     <TextBox    Grid.Row=""0"" Grid.Column=""2"" x:Name=""PART_SearchBox""      />
-                    <TreeView   Grid.Row=""2"" Grid.Column=""0"" x:Name=""PART_VisualTreeView"" />
-                    <ListView   Grid.Row=""2"" Grid.Column=""2"" x:Name=""PART_NodeListView""   />
+                    <TreeView   Grid.Row=""2"" Grid.Column=""0"" x:Name=""PART_VisualTreeView""  ItemsSource=""{TemplateBinding VisualTree}"">
+                        <TreeView.ItemTemplate>
+                            <HierarchicalDataTemplate
+                                ItemsSource=""{Binding Path=Children, Mode=OneTime}""
+                                >
+                                <TextBlock 
+                                    Text=""{Binding Path=Name, Mode=OneTime}""
+                                    ToolTip=""{Binding Path=ToolTip, Mode=OneTime}""
+                                    />
+                            </HierarchicalDataTemplate>
+                        </TreeView.ItemTemplate>
+                    </TreeView>
+                    <DataGrid   Grid.Row=""2"" Grid.Column=""2"" x:Name=""PART_PropertyDataGrid""   ItemsSource=""{Binding ElementName=PART_VisualTreeView,Path=SelectedValue.Properties}"">
+                    </DataGrid>
                 </Grid>
             </ControlTemplate>
         </Setter.Value>
     </Setter>
 </Style>
 ";
-    
+        public class VisualTreeNodeProperty : INotifyPropertyChanged
+        {
+            readonly DependencyObject   DependencyObject    ;
+            readonly DependencyProperty DependencyProperty  ;
+
+            public VisualTreeNodeProperty(
+                DependencyObject dependencyObject, 
+                DependencyProperty dependencyProperty
+                )
+            {
+                DependencyObject    = dependencyObject      ?? new DependencyObject ();
+                DependencyProperty  = dependencyProperty    ;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return DependencyProperty.Name;
+                }
+            }
+
+            public object Value
+            {
+                get
+                {
+                    return DependencyObject.GetValue (DependencyProperty);    
+                }
+                set
+                {
+                    DependencyObject.SetValue (DependencyProperty, value);
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                var handler = PropertyChanged;
+                if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public class VisualTreeNode : INotifyPropertyChanged
+        {
+            readonly DependencyObject   DependencyObject    ;
+            string                      m_name              ;
+            string                      m_tooltip           ;
+            VisualTreeNode[]            m_children          ;
+            VisualTreeNodeProperty[]    m_properties        ;
+
+            public VisualTreeNode(DependencyObject dependencyObject)
+            {
+                DependencyObject = dependencyObject ?? new DependencyObject ();
+            }
+
+            public string Name
+            {
+                get
+                {
+                    if (m_name == null)
+                    {
+                        var type = DependencyObject.GetType ();
+                
+                        var sb = new StringBuilder (128);
+
+                        sb.Append (type.Name);
+
+                        var enumerator = DependencyObject.GetLocalValueEnumerator();
+
+                        while(enumerator.MoveNext())
+                        {
+                            sb.Append (' ');
+                            sb.Append (enumerator.Current.Property.Name);
+                            sb.Append (@"=@""");
+                            sb.Append ((enumerator.Current.Value ?? "").ToString ().Replace(@"""", @""""""));
+                            sb.Append ('"');
+                        }
+                
+                        m_name = sb.ToString ();
+                       
+                    }
+                    return m_name;
+                }
+            }
+
+            public string ToolTip
+            {
+                get
+                {
+                    if (m_tooltip == null)
+                    {
+                        var type = DependencyObject.GetType ();
+                
+                        var sb = new StringBuilder (128);
+
+                        sb.Append (type.Name);
+
+                        var enumerator = DependencyObject.GetLocalValueEnumerator();
+
+                        while(enumerator.MoveNext())
+                        {
+                            sb.AppendLine ();
+                            sb.Append ("  ");
+                            sb.Append (enumerator.Current.Property.Name);
+                            sb.Append (@"=@""");
+                            sb.Append ((enumerator.Current.Value ?? "").ToString ().Replace(@"""", @""""""));
+                            sb.Append ('"');
+                        }
+                
+                        m_tooltip = sb.ToString ();
+                       
+                    }
+                    return m_tooltip;
+                }
+            }
+
+            public VisualTreeNode[] Children
+            {
+                get
+                {
+                    if (m_children == null)
+                    {
+                        m_children = DependencyObject
+                            .GetVisualChildren ()
+                            .Select(d => new VisualTreeNode (d))
+                            .ToArray()
+                            ;
+                    }
+                    return m_children;
+                }
+            }
+
+            static readonly ConcurrentDictionary<Type, DependencyProperty[]> s_cachedProperties = new ConcurrentDictionary<Type, DependencyProperty[]> ();
+
+            public VisualTreeNodeProperty[] Properties
+            {
+                get
+                {
+                    if (m_properties == null)
+                    {
+                        var properties = s_cachedProperties.GetOrAdd (
+                            DependencyObject.GetType(),
+                            type => type
+                                      .GetFields (BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy)
+                                      .Where (fi => fi.FieldType == typeof (DependencyProperty))
+                                      .Select (fi => fi.GetValue (null) as DependencyProperty)
+                                      .Where (dp => dp != null)
+                                      .OrderBy (dp => dp.Name)
+                                      .ToArray ()
+                                      );
+
+                        m_properties = properties
+                            .Select (dp => new VisualTreeNodeProperty (DependencyObject, dp))
+                            .ToArray ()
+                            ;
+                    }
+
+                    return m_properties;
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                var handler = PropertyChanged;      
+                if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
         readonly static Style           s_defaultStyle      ;
 
         static DebugVisualTreeControl ()
@@ -84,7 +277,7 @@ namespace Source.WPF.Debug
             StyleProperty.OverrideMetadata(typeof(DebugVisualTreeControl), new FrameworkPropertyMetadata(s_defaultStyle));
         }
 
-        public DebugVisualTreeControl ()
+        partial void Constructed__DebugVisualTreeControl()
         {
             RoutedEventHandler buttonClick = Button_Click;
             AddHandler (ButtonBase.ClickEvent, buttonClick);
@@ -92,7 +285,7 @@ namespace Source.WPF.Debug
 
         void Button_Click(object sender, RoutedEventArgs e)
         {
-            var button = e.Source as ButtonBase;
+            var button = e.OriginalSource as ButtonBase;
             if (button == null)
             {
                 return;
@@ -103,6 +296,7 @@ namespace Source.WPF.Debug
                 case PART_SelectButton:
                     break;
                 case PART_RefreshButton:
+                    PopulateVisualTree ();
                     break;
                 default:
                     break;
@@ -116,10 +310,25 @@ namespace Source.WPF.Debug
 
         protected override void OnAttachTo(DependencyObject dependencyObject)
         {
+            dependencyObject.Dispatcher.Async_Invoke (
+                "Delay visual tree population till application idle", 
+                PopulateVisualTree
+                );
+        }
+
+        void PopulateVisualTree()
+        {
+            VisualTree.Clear ();
+            if (AttachedTo == null)
+            {
+                return;
+            }
+            VisualTree.Add (new VisualTreeNode (AttachedTo));
         }
 
         protected override void OnDetachFrom(DependencyObject dependencyObject)
         {
+            VisualTree.Clear ();
         }
     }
 }
---
+++ WPF/Debug/Generated_DebugVisualTreeControl_DependencyProperties.cs
@@ -0,0 +1,157 @@
+﻿
+// ############################################################################
+// #                                                                          #
+// #        ---==>  T H I S  F I L E  I S   G E N E R A T E D  <==---         #
+// #                                                                          #
+// # This means that any edits to the .cs file will be lost when its          #
+// # regenerated. Changes should instead be applied to the corresponding      #
+// # template file (.tt)                                                      #
+// ############################################################################
+
+
+
+                                   
+
+// ReSharper disable CompareOfFloatsByEqualityOperator
+// ReSharper disable InconsistentNaming
+// ReSharper disable InvocationIsSkipped
+// ReSharper disable PartialMethodWithSinglePart
+// ReSharper disable PartialTypeWithSinglePart
+// ReSharper disable PossibleUnintendedReferenceComparison
+// ReSharper disable RedundantAssignment
+// ReSharper disable RedundantCast
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable UnusedMember.Local
+
+namespace Source.WPF.Debug
+{
+    using System.Collections;
+    using System.Collections.ObjectModel;
+    using System.Collections.Specialized;
+
+    using System.Windows;
+    using System.Windows.Media;
+
+    // ------------------------------------------------------------------------
+    // DebugVisualTreeControl
+    // ------------------------------------------------------------------------
+    partial class DebugVisualTreeControl
+    {
+        #region Uninteresting generated code
+        static readonly DependencyPropertyKey VisualTreePropertyKey = DependencyProperty.RegisterReadOnly (
+            "VisualTree",
+            typeof (ObservableCollection<VisualTreeNode>),
+            typeof (DebugVisualTreeControl),
+            new FrameworkPropertyMetadata (
+                null,
+                FrameworkPropertyMetadataOptions.None,
+                Changed_VisualTree,
+                Coerce_VisualTree          
+            ));
+
+        public static readonly DependencyProperty VisualTreeProperty = VisualTreePropertyKey.DependencyProperty;
+
+        static void Changed_VisualTree (DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var instance = dependencyObject as DebugVisualTreeControl;
+            if (instance != null)
+            {
+                var oldValue = (ObservableCollection<VisualTreeNode>)eventArgs.OldValue;
+                var newValue = (ObservableCollection<VisualTreeNode>)eventArgs.NewValue;
+
+                if (oldValue != null)
+                {
+                    oldValue.CollectionChanged -= instance.CollectionChanged_VisualTree;
+                }
+
+                if (newValue != null)
+                {
+                    newValue.CollectionChanged += instance.CollectionChanged_VisualTree;
+                }
+
+                instance.Changed_VisualTree (oldValue, newValue);
+            }
+        }
+
+        void CollectionChanged_VisualTree(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            CollectionChanged_VisualTree (
+                sender, 
+                e.Action,
+                e.OldStartingIndex,
+                e.OldItems,
+                e.NewStartingIndex,
+                e.NewItems
+                );
+        }
+
+        static object Coerce_VisualTree (DependencyObject dependencyObject, object basevalue)
+        {
+            var instance = dependencyObject as DebugVisualTreeControl;
+            if (instance == null)
+            {
+                return basevalue;
+            }
+            var value = (ObservableCollection<VisualTreeNode>)basevalue;
+
+            instance.Coerce_VisualTree (ref value);
+
+            if (value == null)
+            {
+               value = new ObservableCollection<VisualTreeNode> ();
+            }
+
+            return value;
+        }
+
+        #endregion
+
+        // --------------------------------------------------------------------
+        // Constructor
+        // --------------------------------------------------------------------
+        public DebugVisualTreeControl ()
+        {
+            CoerceAllProperties ();
+            Constructed__DebugVisualTreeControl ();
+        }
+        // --------------------------------------------------------------------
+        partial void Constructed__DebugVisualTreeControl ();
+        // --------------------------------------------------------------------
+        void CoerceAllProperties ()
+        {
+            CoerceValue (VisualTreeProperty);
+        }
+
+
+        // --------------------------------------------------------------------
+        // Properties
+        // --------------------------------------------------------------------
+
+           
+        // --------------------------------------------------------------------
+        public ObservableCollection<VisualTreeNode> VisualTree
+        {
+            get
+            {
+                return (ObservableCollection<VisualTreeNode>)GetValue (VisualTreeProperty);
+            }
+            private set
+            {
+                if (VisualTree != value)
+                {
+                    SetValue (VisualTreePropertyKey, value);
+                }
+            }
+        }
+        // --------------------------------------------------------------------
+        partial void Changed_VisualTree (ObservableCollection<VisualTreeNode> oldValue, ObservableCollection<VisualTreeNode> newValue);
+        partial void Coerce_VisualTree (ref ObservableCollection<VisualTreeNode> coercedValue);
+        partial void CollectionChanged_VisualTree (object sender, NotifyCollectionChangedAction action, int oldStartingIndex, IList oldItems, int newStartingIndex, IList newItems);
+        // --------------------------------------------------------------------
+
+
+    }
+    // ------------------------------------------------------------------------
+
+}
+                                   
