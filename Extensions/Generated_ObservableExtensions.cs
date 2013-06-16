2175884ee47bdda7672ee740a2b0573f9292ac4e
mrange <marten_range@hotmail.com>
2013-06-16 21:56:44 +0200

    1. Added LINQ-like methods for IObservable<T> in order to improve composability

---
+++ Extensions/Generated_ObservableExtensions.cs
@@ -0,0 +1,268 @@
+﻿// ############################################################################
+// #                                                                          #
+// #        ---==>  T H I S  F I L E  I S   G E N E R A T E D  <==---         #
+// #                                                                          #
+// # This means that any edits to the .cs file will be lost when its          #
+// # regenerated. Changes should instead be applied to the corresponding      #
+// # template file (.tt)                                                      #
+// ############################################################################
+
+
+
+
+
+
+namespace Source.Extensions
+{
+    using System;
+
+
+    sealed partial class WhereObservable<T> : IObservable<T>
+    {
+        readonly IObservable<T> m_observable;
+        readonly Func<T,bool> m_state;
+
+        public WhereObservable (IObservable<T> observable, Func<T,bool> state)
+        {
+            m_observable    = observable;
+            m_state         = state     ;
+        }
+
+        public IDisposable Subscribe (IObserver<T> observer)
+        {
+            var o = new WhereObserver<T> (observer, m_state);
+            return m_observable.Subscribe (o);
+        }
+    }
+
+    sealed partial class WhereObserver<T> : IObserver<T>
+    {
+        readonly IObserver<T> m_observer;
+        Func<T,bool> m_state;
+
+        public WhereObserver (IObserver<T> observer, Func<T,bool> state)
+        {
+            m_observer  = observer  ;
+            m_state     = state     ;
+        }
+
+        partial void Partial_OnNext (T value);
+
+        public void OnNext (T value)
+        {
+            Partial_OnNext (value);
+        }
+
+        public void OnError (Exception error)
+        {
+            m_observer.OnError (error);
+        }
+
+        public void OnCompleted ()
+        {
+            m_observer.OnCompleted ();
+        }
+
+    }
+
+    static partial class ObservableExtensions
+    {
+        public static IObservable<T> Where<T> (IObservable<T> observable, Func<T,bool> p)
+        {
+            if (observable == null)
+            {
+                return EmptyObservable<T>.Value;
+            }
+
+            return new WhereObservable<T> (observable, p);
+        }
+    }
+
+
+    sealed partial class SelectObservable<T,TTo> : IObservable<TTo>
+    {
+        readonly IObservable<T> m_observable;
+        readonly Func<T,TTo> m_state;
+
+        public SelectObservable (IObservable<T> observable, Func<T,TTo> state)
+        {
+            m_observable    = observable;
+            m_state         = state     ;
+        }
+
+        public IDisposable Subscribe (IObserver<TTo> observer)
+        {
+            var o = new SelectObserver<T,TTo> (observer, m_state);
+            return m_observable.Subscribe (o);
+        }
+    }
+
+    sealed partial class SelectObserver<T,TTo> : IObserver<T>
+    {
+        readonly IObserver<TTo> m_observer;
+        Func<T,TTo> m_state;
+
+        public SelectObserver (IObserver<TTo> observer, Func<T,TTo> state)
+        {
+            m_observer  = observer  ;
+            m_state     = state     ;
+        }
+
+        partial void Partial_OnNext (T value);
+
+        public void OnNext (T value)
+        {
+            Partial_OnNext (value);
+        }
+
+        public void OnError (Exception error)
+        {
+            m_observer.OnError (error);
+        }
+
+        public void OnCompleted ()
+        {
+            m_observer.OnCompleted ();
+        }
+
+    }
+
+    static partial class ObservableExtensions
+    {
+        public static IObservable<TTo> Select<T,TTo> (IObservable<T> observable, Func<T,TTo> p)
+        {
+            if (observable == null)
+            {
+                return EmptyObservable<TTo>.Value;
+            }
+
+            return new SelectObservable<T,TTo> (observable, p);
+        }
+    }
+
+
+    sealed partial class TakeObservable<T> : IObservable<T>
+    {
+        readonly IObservable<T> m_observable;
+        readonly int m_state;
+
+        public TakeObservable (IObservable<T> observable, int state)
+        {
+            m_observable    = observable;
+            m_state         = state     ;
+        }
+
+        public IDisposable Subscribe (IObserver<T> observer)
+        {
+            var o = new TakeObserver<T> (observer, m_state);
+            return m_observable.Subscribe (o);
+        }
+    }
+
+    sealed partial class TakeObserver<T> : IObserver<T>
+    {
+        readonly IObserver<T> m_observer;
+        int m_state;
+
+        public TakeObserver (IObserver<T> observer, int state)
+        {
+            m_observer  = observer  ;
+            m_state     = state     ;
+        }
+
+        partial void Partial_OnNext (T value);
+
+        public void OnNext (T value)
+        {
+            Partial_OnNext (value);
+        }
+
+        public void OnError (Exception error)
+        {
+            m_observer.OnError (error);
+        }
+
+        public void OnCompleted ()
+        {
+            m_observer.OnCompleted ();
+        }
+
+    }
+
+    static partial class ObservableExtensions
+    {
+        public static IObservable<T> Take<T> (IObservable<T> observable, int p)
+        {
+            if (observable == null)
+            {
+                return EmptyObservable<T>.Value;
+            }
+
+            return new TakeObservable<T> (observable, p);
+        }
+    }
+
+
+    sealed partial class SkipObservable<T> : IObservable<T>
+    {
+        readonly IObservable<T> m_observable;
+        readonly int m_state;
+
+        public SkipObservable (IObservable<T> observable, int state)
+        {
+            m_observable    = observable;
+            m_state         = state     ;
+        }
+
+        public IDisposable Subscribe (IObserver<T> observer)
+        {
+            var o = new SkipObserver<T> (observer, m_state);
+            return m_observable.Subscribe (o);
+        }
+    }
+
+    sealed partial class SkipObserver<T> : IObserver<T>
+    {
+        readonly IObserver<T> m_observer;
+        int m_state;
+
+        public SkipObserver (IObserver<T> observer, int state)
+        {
+            m_observer  = observer  ;
+            m_state     = state     ;
+        }
+
+        partial void Partial_OnNext (T value);
+
+        public void OnNext (T value)
+        {
+            Partial_OnNext (value);
+        }
+
+        public void OnError (Exception error)
+        {
+            m_observer.OnError (error);
+        }
+
+        public void OnCompleted ()
+        {
+            m_observer.OnCompleted ();
+        }
+
+    }
+
+    static partial class ObservableExtensions
+    {
+        public static IObservable<T> Skip<T> (IObservable<T> observable, int p)
+        {
+            if (observable == null)
+            {
+                return EmptyObservable<T>.Value;
+            }
+
+            return new SkipObservable<T> (observable, p);
+        }
+    }
+
+}
+
---
+++ Extensions/ObservableExtensions.cs
@@ -0,0 +1,111 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+// ### INCLUDE: Generated_ObservableExtensions.cs
+
+namespace Source.Extensions
+{
+    using System;
+
+    sealed partial class EmptyObservable<T> : IObservable<T>
+    {
+        public readonly static EmptyObservable<T> Value = new EmptyObservable<T> (); 
+
+        public IDisposable Subscribe (IObserver<T> observer)
+        {
+            return new ObservableExtensions.EmptyDisposable ();
+        }
+    }
+
+    sealed partial class SingleObserver<T> : IObservable<T>
+    {
+        readonly T m_value;
+
+        public SingleObserver (T value)
+        {
+            m_value = value;
+        }
+
+        public IDisposable Subscribe (IObserver<T> observer)
+        {
+            observer.OnNext (m_value);
+            observer.OnCompleted ();
+            return new ObservableExtensions.EmptyDisposable ();
+        }
+    }
+
+    static partial class ObservableExtensions
+    {
+        public sealed partial class EmptyDisposable : IDisposable
+        {
+            public void Dispose ()
+            {
+            }
+        }
+
+        public static IObservable<T> Empty<T> ()
+        {
+            return EmptyObservable<T>.Value;
+        }
+
+        public static IObservable<T> Single<T> (T value)
+        {
+            return new SingleObserver<T> (value);
+        }
+    }
+
+    partial class WhereObserver<T>
+    {
+        partial void Partial_OnNext (T value)
+        {
+            if (m_state (value))
+            {
+                m_observer.OnNext (value);
+            }
+        }
+    }
+
+    partial class SelectObserver<T, TTo>
+    {
+        partial void Partial_OnNext (T value)
+        {
+            m_observer.OnNext (m_state (value));
+        }
+    }
+
+    partial class TakeObserver<T>
+    {
+        partial void Partial_OnNext (T value)
+        {
+            if (m_state > 0)
+            {
+                --m_state;
+                m_observer.OnNext (value);
+            }
+        }
+    }
+
+    partial class SkipObserver<T>
+    {
+        partial void Partial_OnNext (T value)
+        {
+            if (m_state > 0)
+            {
+                --m_state;
+            }
+            else
+            {
+                m_observer.OnNext (value);
+            }
+        }
+    }
+}
