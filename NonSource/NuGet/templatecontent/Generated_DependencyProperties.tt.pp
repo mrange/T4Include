53928cee7b513b29d40f881e9b28b3c1ec2d8bd2
mrange <marten_range@hotmail.com>
2013-07-17 10:51:31 +0200

    1. Added new nuget package for T4IncludeTemplateSamples

---
+++ NonSource/NuGet/templatecontent/Generated_DependencyProperties.tt.pp
@@ -0,0 +1,23 @@
+﻿<#
+    // Note that the generating code contains extension points using partial methods
+
+    // The namespace the generated code is placed in
+    Namespace   = "$rootnamespace$"         ;   
+
+    Model = new []
+    {
+        // The name of the class that is inheriting DependencyObject
+        new ClassDefinition ("T4IncludeControl")
+        {
+            // A normal property
+            P ("bool"   ,   "IsValid"           ),
+            // An attached property
+            P ("string" ,   "AttachedValue"     , flags:PropertyFlags.IsAttached    )
+        },
+    };
+
+#>
+
+<#@ include file="$(SolutionDir)\packages\T4IncludeTemplate.1.0.1\T4\DependencyProperties.ttinclude" #>
+
+
